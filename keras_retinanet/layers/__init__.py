9e492d08de4a2e5217e744cf7fad88904e638ede
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2017-09-20 08:54:24 +0200

    Move upsampling to separate layer.

--- keras_retinanet/layers/__init__.py
+++ keras_retinanet/layers/__init__.py
@@ -1,4 +1,5 @@
 from ._anchor_target import AnchorTarget
 from ._focal_loss import FocalLoss
-from ._regress_boxes import RegressBoxes
 from ._non_maximum_suppression import NonMaximumSuppression
+from ._regress_boxes import RegressBoxes
+from ._upsampling import Upsampling
