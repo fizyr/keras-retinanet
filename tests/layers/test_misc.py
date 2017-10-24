90a94ec0537667625af19b45d20285559a769e08
Hans Gaiser <hansg91@gmail.com>
2017-10-24 22:59:00 +0200

    Add Anchors unit tests.

---
+++ tests/layers/test_misc.py
@@ -0,0 +1,95 @@
+import keras
+import keras_retinanet.layers
+
+import numpy as np
+
+
+class TestAnchors(object):
+    def test_simple(self):
+        # create simple Anchors layer
+        anchors_layer = keras_retinanet.layers.Anchors(
+            size=32,
+            stride=8,
+            ratios=np.array([1,], keras.backend.floatx()),
+            scales=np.array([1,], keras.backend.floatx()),
+        )
+
+        # create fake features input (only shape is used anyway)
+        features = np.zeros((1, 2, 2, 1024))
+        features = keras.backend.variable(features)
+
+        # call the Anchors layer
+        anchors = anchors_layer.call(features)
+        anchors = keras.backend.eval(anchors)
+
+        # expected anchor values
+        expected = np.array([[
+            [-12, -12, 20, 20],
+            [-4 , -12, 28, 20],
+            [-12, -4 , 20, 28],
+            [-4 , -4 , 28, 28],
+        ]], dtype=keras.backend.floatx())
+
+        # test anchor values
+        np.testing.assert_array_equal(anchors, expected)
+
+    def test_mini_batch(self):
+        # create simple Anchors layer
+        anchors_layer = keras_retinanet.layers.Anchors(
+            size=32,
+            stride=8,
+            ratios=np.array([1,], keras.backend.floatx()),
+            scales=np.array([1,], keras.backend.floatx()),
+        )
+
+        # create fake features input with batch_size=2
+        features = np.zeros((2, 2, 2, 1024))
+        features = keras.backend.variable(features)
+
+        # call the Anchors layer
+        anchors = anchors_layer.call(features)
+        anchors = keras.backend.eval(anchors)
+
+        # expected anchor values
+        expected = np.array([[
+            [-12, -12, 20, 20],
+            [-4 , -12, 28, 20],
+            [-12, -4 , 20, 28],
+            [-4 , -4 , 28, 28],
+        ]], dtype=keras.backend.floatx())
+        expected = np.tile(expected, (2, 1, 1))
+
+        # test anchor values
+        np.testing.assert_array_equal(anchors, expected)
+
+
+class TestTensorReshape(object):
+    def test_simple(self):
+        pass
+
+    def test_mini_batch(self):
+        pass
+
+
+class TestNonMaximumSuppression(object):
+    def test_simple(self):
+        pass
+
+    def test_mini_batch(self):
+        pass
+
+
+class TestUpsampleLike(object):
+    def test_simple(self):
+        pass
+
+    def test_mini_batch(self):
+        pass
+
+
+class TestRegressBoxe(object):
+    def test_simple(self):
+        pass
+
+    def test_mini_batch(self):
+        pass
