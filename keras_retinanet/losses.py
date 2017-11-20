aae96334839483a983f72ea66d6e5e2ad5590543
yhenon <yannhenon@gmail.com>
2017-11-20 15:04:58 -0500

    removed debug info

--- keras_retinanet/losses.py
+++ keras_retinanet/losses.py
@@ -18,37 +18,6 @@ import keras
 import keras_retinanet
 
 
-
-def focal(alpha=0.25, gamma=2.0):
-    def _focal(y_true, y_pred):
-        # discard batches, throw all labels / classifications on one big blob
-        labels         = keras.backend.reshape(y_true, (-1, keras.backend.shape(y_true)[2]))
-        classification = keras.backend.reshape(y_pred, (-1, keras.backend.shape(y_pred)[2]))
-
-        # filter out "ignore" anchors
-        anchor_state   = keras.backend.max(labels, axis=1)  # -1 for ignore, 0 for background, 1 for object
-        indices        = keras_retinanet.backend.where(keras.backend.not_equal(anchor_state, -1))
-        classification = keras_retinanet.backend.gather_nd(classification, indices)
-        labels         = keras_retinanet.backend.gather_nd(labels, indices)
-        anchor_state   = keras_retinanet.backend.gather_nd(anchor_state, indices)
-
-        # select classification scores for labeled anchors
-        alpha_factor = keras.backend.ones_like(labels) * alpha
-        alpha_factor = keras_retinanet.backend.where(keras.backend.equal(labels, 1), alpha_factor, 1 - alpha_factor)
-        focal_weight = keras_retinanet.backend.where(keras.backend.equal(labels, 1), 1 - classification, classification)
-        focal_weight = alpha_factor * focal_weight ** gamma
-
-        cls_loss = focal_weight * keras.backend.binary_crossentropy(labels, classification)
-        cls_loss = keras.backend.sum(cls_loss)
-
-        # "The total focal loss of an image is computed as the sum
-        # of the focal loss over all ~100k anchors, normalized by the
-        # number of anchors assigned to a ground-truth box."
-        cls_loss = cls_loss / (keras.backend.maximum(1.0, keras.backend.sum(anchor_state)))
-        return cls_loss
-
-    return _focal
-'''
 def focal(alpha=0.25, gamma=2.0):
     def _focal(y_true, y_pred):
 
@@ -88,7 +57,6 @@ def focal(alpha=0.25, gamma=2.0):
         return keras.backend.sum(cls_loss)/keras.backend.cast(keras.backend.shape(y_true)[0], keras.backend.floatx())
 
     return _focal
-'''
 
 def smooth_l1(sigma=3.0):
     sigma_squared = sigma ** 2
