92ba5ef7569202a6bc19c2556a044087947e1f43
Maarten de Vries <maarten@de-vri.es>
2017-11-26 21:23:40 +0100

    Make sure ratios and scales are numpy arrays. (#109)

--- keras_retinanet/layers/_misc.py
+++ keras_retinanet/layers/_misc.py
@@ -30,8 +30,12 @@ class Anchors(keras.layers.Layer):
 
         if ratios is None:
             self.ratios  = np.array([0.5, 1, 2], keras.backend.floatx()),
+        elif isinstance(ratios, list):
+            self.ratios  = np.array(ratios)
         if scales is None:
             self.scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], keras.backend.floatx()),
+        elif isinstance(scales, list):
+            self.scales  = np.array(scales)
 
         self.num_anchors = len(ratios) * len(scales)
         self.anchors     = keras.backend.variable(keras_retinanet.utils.anchors.generate_anchors(
