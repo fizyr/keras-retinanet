5666e064357e674ead47611a28e6dc24da1c550c
Maarten de Vries <maarten@de-vri.es>
2017-12-13 21:30:04 +0100

    utils.transform: Add random_transform and friends.

--- keras_retinanet/utils/transform.py
+++ keras_retinanet/utils/transform.py
@@ -123,3 +123,104 @@ def transform_around(transform, center):
         translate(center) * transform * translate(-center)
     """
     return np.dot(np.dot(translation(center), transform), translation(-center))
+
+
+def random_transform(
+    min_rotation,
+    max_rotation,
+    min_translation,
+    max_translation,
+    min_shear,
+    max_shear,
+    min_scaling,
+    max_scaling,
+    flip_x_chance,
+    flip_y_chance,
+    prng = DEFAULT_PRNG
+):
+    """ Create a random transformation.
+
+    The transformation consists of the following operations in this order (from left to right):
+      * rotation
+      * translation
+      * shear
+      * scaling
+      * flip x (if applied)
+      * flip y (if applied)
+
+    # Arguments
+        min_rotation:    The minimum rotation for the transform as scalar.
+        max_rotation:    The maximum rotation for the transform as scalar.
+        min_translation: The minimum translation for the transform as 2D column vector.
+        max_translation: The maximum translation for the transform as 2D column vector.
+        min_shear:       The minimum shear for the transform as scalar.
+        max_shear:       The maximum shear for the transform as scalar.
+        min_scaling:     The minimum scaling for the transform as 2D column vector.
+        max_scaling:     The maximum scaling for the transform as 2D column vector.
+        flip_x_chance:   The chance (0 to 1) that a transform will contain a flip along X direction.
+        flip_y_chance:   The chance (0 to 1) that a transform will contain a flip along Y direction.
+        prng:            The pseudo-random number generator to use.
+    """
+    result = random_rotation(min_rotation, max_rotation, prng)
+    result = np.dot(result, random_translation(min_translation, max_translation, prng))
+    result = np.dot(result, random_shear(min_shear, max_shear, prng))
+    result = np.dot(result, random_scaling(min_scaling, max_scaling, prng))
+    flip_x = prng.uniform(0, 1) < flip_x_chance
+    flip_y = prng.uniform(0, 1) < flip_y_chance
+    result = np.dot(result, scaling(vec2(-1 if flip_x else 1, -1 if flip_y else 1)))
+    return result
+
+
+def random_transform_generator(*args, **kwargs):
+    """ Create a random transform generator with the same arugments as `random_transform`. """
+    while True:
+        yield random_transform(*args, **kwargs)
+
+
+def _translate_image_data_generator_params(image_data_generator):
+    """ Translate the properties of a Keras ImageDataGenerator to keyword arguments for random_transform() """
+
+
+def random_transform_from_image_data_generator(image_data_generator, prng = DEFAULT_PRNG):
+    """ Create a random transform using the same parameters as a Keras ImageDataGenerator.
+
+    Note that the image dimensions are unknown at this points,
+    so the transform origin should be modified to the image center before using it.
+    Additionally, the translation is relative to the image size.
+    You can use `transform_image` to fix these details for you.
+
+    # Arguments
+        image_data_generator: The Keras ImageDataGenerator to mimick.
+        prng:                 The speudo-random number generator to use.
+    """
+    rotation    = image_data_generator.rotation_range
+    translation = np.array([[image_data_generator.width_shift_range], [image_data_generator.height_shift_range]])
+    shear       = image_data_generator.shear_range
+    min_zoom    = image_data_generator.zoom_range[0]
+    max_zoom    = image_data_generator.zoom_range[1]
+    flip_x      = 0.5 if image_data_generator.horizontal_flip else 0
+    flip_y      = 0.5 if image_data_generator.vertical_flip else 0
+
+    return random_transform(
+        min_rotation    = -rotation,
+        max_rotation    = +rotation,
+        min_translation = -translation,
+        max_translation = +translation,
+        min_shear       = -shear,
+        max_shear       = +shear,
+        min_scaling     = vec2(min_zoom, min_zoom),
+        max_scaling     = vec2(max_zoom, max_zoom),
+        flip_x_chance   = flip_x,
+        flip_y_chance   = flip_y,
+        prng            = prng
+    )
+
+
+def random_transform_generator_from_image_data_generator(image_data_generator, prng = DEFAULT_PRNG):
+    """ Create a random transform generator that mimicks a Keras ImageDataGenerator.
+    # Arguments
+        image_data_generator: The Keras ImageDataGenerator to mimick.
+        prng:                 The speudo-random number generator to use.
+    """
+    while True:
+        yield random_transform_from_image_data_generator(image_data_generator, prng)
