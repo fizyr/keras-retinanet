1723ccfca40cbacd24765855a8df4f28abf5725a
Ashley Williamson <awilliamson@lincoln.ac.uk>
2017-12-18 14:28:23 +0000

    Reworked VOC Evaluation into class

--- keras_retinanet/utils/voc_eval.py
+++ keras_retinanet/utils/voc_eval.py
@@ -5,96 +5,114 @@ import numpy as np
 import pathlib
 import cv2
 
-def load_ground_truths( generator, image_id ):
-    gt=[]
-    for annotation in generator.load_annotations(image_id):
-        label = int(annotation[4])
-        gt.append({
-            'image_id': generator.image_names[image_id],
-            'category_id': generator.label_to_name(label), # Use name
-            'bbox': (annotation[:4]).tolist()
-        })
-    return gt
+class VOCEvaluator():
+    def __init__(self, generator, model, threshold=0.05, save=False, save_path='images_voc'):
+        self.generator = generator
+        self.model = model
+        self.threshold = threshold
+        self.save = save
 
-def draw_gt_bboxes(image, anno_list):
-    for anno in anno_list:
-        label = anno['category_id']
-        b = np.array(anno['bbox']).astype(int)
+        self.detections = []
 
-        cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), (0, 255, 0), 1)
-        caption = "GT:{}".format(label)
-        cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 3)
-        cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+        if self.save:
+            self.save_path = pathlib.Path(save_path)
+            self.save_path.mkdir(exist_ok=True)
 
-def filter_top_N_bboxes(bbox_preds, N):
-    max_liklihood_sorted = sorted(bbox_preds, key=lambda x: x['score'], reverse=True)
-    return max_liklihood_sorted[:min(len(max_liklihood_sorted), N)]
+    def load_ground_truths(self, image_id ):
+        gt=[]
+        for annotation in self.generator.load_annotations(image_id):
+            label = int(annotation[4])
+            gt.append({
+                'image_id': self.generator.image_names[image_id],
+                'category_id': self.generator.label_to_name(label), # Use name
+                'bbox': (annotation[:4]).tolist()
+            })
+        return gt
 
-def draw_top_N(image, bbox_preds, generator, N):
-    for bbox in filter_top_N_bboxes(bbox_preds, N):
-        b = np.array(bbox['bbox']).astype(int)
-        cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
-        caption = "P:{} {:.3f}".format(generator.label_to_name(bbox['category_id']), bbox[
-            'score'])
-        cv2.putText(image, caption, (b[0], b[3] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 3)
-        cv2.putText(image, caption, (b[0], b[3] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+    @staticmethod
+    def draw_gt_bboxes(image, anno_list):
+        for anno in anno_list:
+            label = anno['category_id']
+            b = np.array(anno['bbox']).astype(int)
 
-def process_detections(dets, image_id, threshold, generator):
-    filtered_detections = []
-    for detection in dets[0, :, :]:
-        positive_labels = np.where(detection[4:] > threshold)[0]
+            cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), (0, 255, 0), 1)
+            caption = "GT:{}".format(label)
+            cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 3)
+            cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
 
-        # Skip as we have no positive detections above the threshold for this image
-        if len(positive_labels) < 1:
-            continue
+    @staticmethod
+    def filter_top_N_bboxes(bbox_preds, N):
+        max_liklihood_sorted = sorted(bbox_preds, key=lambda x: x['score'], reverse=True)
+        return max_liklihood_sorted[:min(len(max_liklihood_sorted), N)]
 
-        # append detections for each positively labeled class
-        for indx, label in enumerate(positive_labels):
-            image_result = {
-                'image_id': generator.image_names[image_id],
-                'category_id': label,
-                'score': float(detection[4 + label]),
-                'bbox': (detection[:4]).tolist(),
-            }
-
-            filtered_detections.append(image_result)
-
-    return filtered_detections
-
-def evaluate_voc(generator, model, threshold=0.05, save=False, save_path='images_voc'):
-
-    if save:
-        dest = pathlib.Path(save_path)
-        dest.mkdir(exist_ok=True)
-
-    gt = [[] for _ in range(generator.size())]
-
-    for i in range(generator.size()):
-        image = generator.load_image(i)
-        draw = image.copy()
-
-        image, scale = generator.resize_image(image)
-        image = generator.preprocess_image(image)
-
-        _, _, det = model.predict_on_batch(np.expand_dims(image, axis=0))
-
-        gt[i] = load_ground_truths(generator, i)
-
+    @staticmethod
+    def preprocess_detection(det, scale, image):
         det[0, :, :4] /= scale
         det[:, :, 0] = np.maximum(0, det[:, :, 0])
         det[:, :, 1] = np.maximum(0, det[:, :, 1])
         det[:, :, 2] = np.minimum(image.shape[1], det[:, :, 2])
         det[:, :, 3] = np.minimum(image.shape[0], det[:, :, 3])
 
-        results_this_image = process_detections(det, i, threshold, generator)
+    def draw_top_N(self, image, bbox_preds, N):
+        for bbox in self.filter_top_N_bboxes(bbox_preds, N):
+            b = np.array(bbox['bbox']).astype(int)
+            cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+            caption = "P:{} {:.3f}".format(self.generator.label_to_name(bbox['category_id']), bbox[
+                'score'])
+            cv2.putText(image, caption, (b[0], b[3] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 3)
+            cv2.putText(image, caption, (b[0], b[3] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
 
-        # Draw Top N BBox predictions!
-        draw_top_N(draw, results_this_image, generator, 5)
+    def process_image_detections(self, dets, image_id):
+        filtered_detections = []
+        for detection in dets[0, :, :]:
+            positive_labels = np.where(detection[4:] > self.threshold)[0]
 
-        # Draw Ground Truth
-        draw_gt_bboxes(draw, gt[i])
+            # Skip as we have no positive detections above the threshold for this image
+            if len(positive_labels) < 1:
+                continue
 
-        if save:
-            cv2.imwrite(str(dest.joinpath("{}.jpg".format(i))), draw)
+            # append detections for each positively labeled class
+            for indx, label in enumerate(positive_labels):
+                image_result = {
+                    'image_id': self.generator.image_names[image_id],
+                    'category_id': label,
+                    'score': float(detection[4 + label]),
+                    'bbox': (detection[:4]).tolist(),
+                }
 
-        print('{}/{}'.format(i, len(generator.image_names)), end='\r')
+                filtered_detections.append(image_result)
+
+        return filtered_detections
+
+    def save_image(self, image, image_name):
+        cv2.imwrite(str(self.save_path.joinpath(image_name)), image)
+
+    def evaluate(self):
+        gt = [[] for _ in range(self.generator.size())]
+
+        for i in range(self.generator.size()):
+
+            image = self.generator.load_image(i)
+            gt[i] = self.load_ground_truths(i)
+
+            draw = image.copy()
+
+            image, scale = self.generator.resize_image(image)
+            image = self.generator.preprocess_image(image)
+
+            _, _, det = self.model.predict_on_batch(np.expand_dims(image, axis=0))
+
+            self.preprocess_detection(det,scale,image)
+
+            results_this_image = self.process_image_detections(det, i)
+
+            # Draw Top N BBox predictions!
+            self.draw_top_N(draw, results_this_image, 5)
+
+            # Draw Ground Truth
+            self.draw_gt_bboxes(draw, gt[i])
+
+            if self.save:
+                self.save_image(draw, "{}.jpg".format(i))
+
+            print('{}/{}'.format(i, self.generator.size()), end='\r')
