e88eaecbb133795fcafa055bbbd5b990d9327a77
Ashley Williamson <awilliamson@lincoln.ac.uk>
2018-02-06 21:55:22 +0000

    Base plugin interface

---
+++ keras_retinanet/plugins/plugin.py
@@ -0,0 +1,15 @@
+class Plugin():
+    def __init__(self):
+        pass
+
+    def preload(self):
+        pass
+
+    def postload(self):
+        pass
+
+    def parser_args(self):
+        pass
+
+    def get_generator(self):
+        pass
