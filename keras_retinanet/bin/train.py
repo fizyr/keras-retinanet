43319c89bfe5866a96362c5e6c5dafddf0024515
Ashley Williamson <awilliamson@lincoln.ac.uk>
2018-02-12 15:56:06 +0000

    Move import to script-level
    Manager required in additional places therefore cannot be
    function-scoped

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -17,6 +17,7 @@ limitations under the License.
 """
 
 import argparse
+from yapsy.PluginManager import PluginManagerSingleton
 import os
 import sys
 
@@ -303,7 +304,6 @@ def parse_args(args):
     return check_args(parser.parse_args(args))
 
 def load_plugins(plugin_path):
-    from yapsy.PluginManager import PluginManagerSingleton
 
     pm = PluginManagerSingleton.get()
     pl = pm.getPluginLocator()
