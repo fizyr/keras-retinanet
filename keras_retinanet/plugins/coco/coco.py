0835a5a6a0648a2e064522d2c7555cf6da274ade
Ashley Williamson <awilliamson@lincoln.ac.uk>
2018-02-12 13:48:36 +0000

    Add VOC and COCO Dataset Plugin definitions

---
+++ keras_retinanet/plugins/coco/coco.py
@@ -0,0 +1,23 @@
+"""
+Copyright 2017-2018 Ashley Williamson (https://inp.io)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import keras_retinanet.utils.plugin as plugins
+# See https://yapsy.readthedocs.io/en/latest/Advices.html#plugin-class-detection-caveat
+# Caveat surrounding import. Must us 'as' rather than directly importing DatasetPlugin
+
+class CocoPlugin(plugins.DatasetPlugin):
+    def __init__(self):
+        super(CocoPlugin, self).__init__()
