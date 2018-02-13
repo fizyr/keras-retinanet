3f79156d3f32b5dafb0d2c193fc17a4f03420170
Ashley Williamson <awilliamson@lincoln.ac.uk>
2018-02-13 15:13:21 +0000

    Add generators implementation to plugins
    COCO, VOC, and CSV plugins now have implementation for get_generators.
    Returns a dictionary to support more extensible returns from future
    Datasets.

--- keras_retinanet/plugins/coco/coco.py
+++ keras_retinanet/plugins/coco/coco.py
@@ -18,6 +18,9 @@ import keras_retinanet.utils.plugin as plugins
 # See https://yapsy.readthedocs.io/en/latest/Advices.html#plugin-class-detection-caveat
 # Caveat surrounding import. Must us 'as' rather than directly importing DatasetPlugin
 
+from keras_retinanet.preprocessing.coco import CocoGenerator
+
+
 class CocoPlugin(plugins.DatasetPlugin):
     def __init__(self):
         super(CocoPlugin, self).__init__()
@@ -28,3 +31,23 @@ class CocoPlugin(plugins.DatasetPlugin):
         coco_parser.add_argument('coco_path', help='Path to dataset directory (ie. /tmp/COCO).')
 
         return coco_parser
+
+    def create_generators(self, args, transform_generator=None):
+        train_generator = CocoGenerator(
+            args.coco_path,
+            'train2017',
+            transform_generator=transform_generator,
+            batch_size=args.batch_size
+        )
+
+        validation_generator = CocoGenerator(
+            args.coco_path,
+            'val2017',
+            batch_size=args.batch_size
+        )
+
+        return {
+            "train_generator": train_generator,
+            "validation_generator": validation_generator
+        }
+
