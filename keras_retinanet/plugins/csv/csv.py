d8781112f89dffdd604287eb0f7e2bacf28649ee
Ashley Williamson <awilliamson@lincoln.ac.uk>
2018-02-13 15:13:21 +0000

    Add generators implementation to plugins
    COCO, VOC, and CSV plugins now have implementation for get_generators.
    Returns a dictionary to support more extensible returns from future
    Datasets.

--- keras_retinanet/plugins/csv/csv.py
+++ keras_retinanet/plugins/csv/csv.py
@@ -18,6 +18,9 @@ import keras_retinanet.utils.plugin as plugins
 # See https://yapsy.readthedocs.io/en/latest/Advices.html#plugin-class-detection-caveat
 # Caveat surrounding import. Must us 'as' rather than directly importing DatasetPlugin
 
+from keras_retinanet.preprocessing.csv_generator import CSVGenerator
+
+
 class CSVPlugin(plugins.DatasetPlugin):
     def __init__(self):
         super(CSVPlugin, self).__init__()
@@ -31,3 +34,25 @@ class CSVPlugin(plugins.DatasetPlugin):
                                 help='Path to CSV file containing annotations for validation (optional).')
 
         return csv_parser
+
+    def create_generators(self, args, transform_generator=None):
+        train_generator = CSVGenerator(
+            args.annotations,
+            args.classes,
+            transform_generator=transform_generator,
+            batch_size=args.batch_size
+        )
+
+        if args.val_annotations:
+            validation_generator = CSVGenerator(
+                args.val_annotations,
+                args.classes,
+                batch_size=args.batch_size
+            )
+        else:
+            validation_generator = None
+
+        return {
+            "train_generator": train_generator,
+            "validation_generator": validation_generator
+        }
--- keras_retinanet/plugins/voc/voc.py
+++ keras_retinanet/plugins/voc/voc.py
@@ -18,6 +18,9 @@ import keras_retinanet.utils.plugin as plugins
 # See https://yapsy.readthedocs.io/en/latest/Advices.html#plugin-class-detection-caveat
 # Caveat surrounding import. Must us 'as' rather than directly importing DatasetPlugin
 
+from keras_retinanet.preprocessing.pascal_voc import PascalVocGenerator
+
+
 class VocPlugin(plugins.DatasetPlugin):
     def __init__(self):
         super(VocPlugin, self).__init__()
@@ -29,3 +32,22 @@ class VocPlugin(plugins.DatasetPlugin):
         pascal_parser.add_argument('pascal_path', help='Path to dataset directory (ie. /tmp/VOCdevkit).')
 
         return pascal_parser
+
+    def create_generators(self, args, transform_generator=None):
+        train_generator = PascalVocGenerator(
+            args.pascal_path,
+            'trainval',
+            transform_generator=transform_generator,
+            batch_size=args.batch_size
+        )
+
+        validation_generator = PascalVocGenerator(
+            args.pascal_path,
+            'test',
+            batch_size=args.batch_size
+        )
+
+        return {
+            "train_generator": train_generator,
+            "validation_generator": validation_generator
+        }
