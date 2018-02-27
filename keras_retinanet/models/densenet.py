bfa9e879ebb209c7eec4c98ee7a6ae65a23530c7
András Vidosits <vidosits.andras@gmail.com>
2018-02-27 12:31:31 +0100

    Fixed path for densenet weights

--- keras_retinanet/models/densenet.py
+++ keras_retinanet/models/densenet.py
@@ -19,7 +19,7 @@ from keras.applications.densenet import DenseNet, get_file
 
 from ..models import retinanet
 
-WEIGHT_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.8/densenet{}_weights_tf_dim_ordering_tf_kernels_notop.h5'
+WEIGHT_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.8/{}_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 custom_objects = retinanet.custom_objects
 
@@ -39,9 +39,8 @@ def download_imagenet(backbone):
     if keras.backend.image_data_format() == 'channels_first':
         raise ValueError('Weights for "channels_first" format are not available.')
 
-    model_name = '{}_weights_tf_dim_ordering_tf_kernels_notop.h5'.format(backbone)
-    weights_url = WEIGHT_PATH.format(model_name)
-    weights_path = get_file(model_name, weights_url, cache_subdir='models')
+    weights_url = WEIGHT_PATH.format(backbone)
+    weights_path = get_file(weights_url, weights_url, cache_subdir='models')
 
     return weights_path
 
