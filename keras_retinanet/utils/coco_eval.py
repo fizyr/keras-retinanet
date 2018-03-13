f004b77e513d36e29f53e3154866f5f61217b9be
jjiunlin <33717622+jjiunlin@users.noreply.github.com>
2018-03-13 19:28:50 +0800

    Update coco_eval.py

--- keras_retinanet/utils/coco_eval.py
+++ keras_retinanet/utils/coco_eval.py
@@ -84,6 +84,6 @@ def evaluate_coco(generator, model, threshold=0.05):
     coco_eval.params.imgIds = image_ids
     coco_eval.evaluate()
     coco_eval.accumulate()
-    coco_eval.summarize() 
+    coco_eval.summarize()
     mAP = coco_eval.stats
     return mAP
