b6a965f9eb71a6c4a0ae04154429d19a13792336
Alexander Pacha <alexander.pacha@gmail.com>
2018-05-22 11:31:37 +0200

    Disabling mAP monitoring by default, because not everyone might have an evaluation running

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -141,9 +141,9 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
                 '{backbone}_{dataset_type}_{{epoch:02d}}.h5'.format(backbone=args.backbone, dataset_type=args.dataset_type)
             ),
             verbose=1,
-            save_best_only=True,
-            monitor="mAP",
-            mode='max'
+            # save_best_only=True,
+            # monitor="mAP",
+            # mode='max'
         )
         checkpoint = RedirectModel(checkpoint, model)
         callbacks.append(checkpoint)
