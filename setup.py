71627a029ccb4932ff0c89679641808f0b148d5a
Hans Gaiser <h.gaiser@fizyr.com>
2018-07-18 16:01:50 +0200

    Use setuptools>=18.0 for Cython code.
    https://setuptools.readthedocs.io/en/latest/history.html?highlight=cython#id328

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import setuptools
-from Cython.Build import cythonize
 from setuptools.extension import Extension
 import numpy as np
 
@@ -12,17 +11,17 @@ extensions = [
 ]
 
 setuptools.setup(
-    name='keras-retinanet',
-    version='0.4.0',
-    description='Keras implementation of RetinaNet object detection.',
-    url='https://github.com/fizyr/keras-retinanet',
-    author='Hans Gaiser',
-    author_email='h.gaiser@fizyr.com',
-    maintainer='Hans Gaiser',
-    maintainer_email='h.gaiser@fizyr.com',
-    packages=setuptools.find_packages(),
-    install_requires=['keras', 'keras-resnet', 'six', 'scipy', 'cython'],
-    entry_points = {
+    name             = 'keras-retinanet',
+    version          = '0.4.0',
+    description      = 'Keras implementation of RetinaNet object detection.',
+    url              = 'https://github.com/fizyr/keras-retinanet',
+    author           = 'Hans Gaiser',
+    author_email     = 'h.gaiser@fizyr.com',
+    maintainer       = 'Hans Gaiser',
+    maintainer_email = 'h.gaiser@fizyr.com',
+    packages         = setuptools.find_packages(),
+    install_requires = ['keras', 'keras-resnet', 'six', 'scipy', 'cython'],
+    entry_points     = {
         'console_scripts': [
             'retinanet-train=keras_retinanet.bin.train:main',
             'retinanet-evaluate=keras_retinanet.bin.evaluate:main',
@@ -30,5 +29,6 @@ setuptools.setup(
             'retinanet-convert-model=keras_retinanet.bin.convert_model:main',
         ],
     },
-    ext_modules = cythonize(extensions)
+    ext_modules    = extensions,
+    setup_requires = ["cython>=0.28"]
 )
