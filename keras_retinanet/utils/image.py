faa8412c3dae753e17c8081f3b9da691ae8cf9f1
Enrico Liscio <e.liscio@fizyr.com>
2018-10-12 11:28:59 +0200

    Split image resizing in utils.

--- keras_retinanet/utils/image.py
+++ keras_retinanet/utils/image.py
@@ -150,17 +150,17 @@ def apply_transform(matrix, image, params):
     return output
 
 
-def resize_image(img, min_side=800, max_side=1333):
-    """ Resize an image such that the size is constrained to min_side and max_side.
+def compute_resize_scale(image_shape, min_side=800, max_side=1333):
+    """ Compute an image scale such that the image size is constrained to min_side and max_side.
 
     Args
         min_side: The image's min side will be equal to min_side after resizing.
         max_side: If after resizing the image's max side is above max_side, resize until the max side is equal to max_side.
 
     Returns
-        A resized image.
+        A resizing scale.
     """
-    (rows, cols, _) = img.shape
+    (rows, cols, _) = image_shape
 
     smallest_side = min(rows, cols)
 
@@ -172,6 +172,22 @@ def resize_image(img, min_side=800, max_side=1333):
     largest_side = max(rows, cols)
     if largest_side * scale > max_side:
         scale = max_side / largest_side
+
+    return scale
+
+
+def resize_image(img, min_side=800, max_side=1333):
+    """ Resize an image such that the size is constrained to min_side and max_side.
+
+    Args
+        min_side: The image's min side will be equal to min_side after resizing.
+        max_side: If after resizing the image's max side is above max_side, resize until the max side is equal to max_side.
+
+    Returns
+        A resized image.
+    """
+    # compute scale to resize the image
+    scale = compute_resize_scale(img.shape, min_side=min_side, max_side=max_side)
 
     # resize the image with the computed scale
     img = cv2.resize(img, None, fx=scale, fy=scale)
