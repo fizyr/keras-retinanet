ab3383b46b8afab116ea938bca0059bcbfd987ee
Valeriu Lacatusu <lacatusu.valeriu@gmail.com>
2018-11-10 19:06:21 +0100

    Small questioning ...
    Wouldnt it be better to print both mAP values since the evaluation takes some time already for some datasets? It will take twice this time if we want to compute these values separately... (this is happening too me right now and I really start to find this annoying :) ).

--- keras_retinanet/bin/evaluate.py
+++ keras_retinanet/bin/evaluate.py
@@ -108,7 +108,6 @@ def parse_args(args):
     parser.add_argument('--image-min-side',   help='Rescale the image so the smallest side is min_side.', type=int, default=800)
     parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file (only used with --convert-model).')
-    parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
 
     return parser.parse_args(args)
 
@@ -181,10 +180,8 @@ def main(args=None):
             print('No test instances found.')
             return
 
-        if args.weighted_average:
-            print('mAP: {:.4f}'.format(sum([a * b for a, b in zip(total_instances, precisions)]) / sum(total_instances)))
-        else:
-            print('mAP: {:.4f}'.format(sum(precisions) / sum(x > 0 for x in total_instances)))
+        print('mAP using the weighted average of precisions among classes: {:.4f}'.format(sum([a * b for a, b in zip(total_instances, precisions)]) / sum(total_instances)))
+        print('mAP: {:.4f}'.format(sum(precisions) / sum(x > 0 for x in total_instances)))
 
 
 if __name__ == '__main__':
