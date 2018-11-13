610a122959fbe91d60b25ad4609cb60e36c14bea
Maxim Claeys <maxim.claeys@robovision.eu>
2018-11-13 15:03:16 +0100

    Remove numpy import in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
 from setuptools.extension import Extension
-import numpy as np
+
+class custom_wrapper(object):
+    def __new__(cls, *args, **kwargs):
+        from setuptools.command.build_ext import build_ext
+        class custom_build_ext(build_ext, object):
+            def run(self):
+                import numpy as np
+                self.include_dirs.append(np.get_include())
+                build_ext.run(self)
+        return custom_build_ext(*args, **kwargs)
 
 extensions = [
     Extension(
         'keras_retinanet.utils.compute_overlap',
-        ['keras_retinanet/utils/compute_overlap.pyx'],
-        include_dirs=[np.get_include()]
+        ['keras_retinanet/utils/compute_overlap.pyx']
     ),
 ]
 
@@ -19,6 +27,7 @@ setuptools.setup(
     author_email     = 'h.gaiser@fizyr.com',
     maintainer       = 'Hans Gaiser',
     maintainer_email = 'h.gaiser@fizyr.com',
+    cmdclass         = {'build_ext': custom_wrapper},
     packages         = setuptools.find_packages(),
     install_requires = ['keras', 'keras-resnet', 'six', 'scipy', 'cython', 'Pillow', 'opencv-python', 'progressbar2'],
     entry_points     = {
@@ -30,5 +39,5 @@ setuptools.setup(
         ],
     },
     ext_modules    = extensions,
-    setup_requires = ["cython>=0.28"]
+    setup_requires = ["cython>=0.28", "numpy>=1.14.0"]
 )
