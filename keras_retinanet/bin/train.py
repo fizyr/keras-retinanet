a4b5ecc43bd9325424c4f28b26619460dc234cca
dshahrokhian <dani.shahrokhian@gmail.com>
2019-02-27 12:24:24 +0100

    Add validation loss computation as optional argument

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -410,6 +410,7 @@ def parse_args(args):
     parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
     parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
+    parser.add_argument('--compute-val-loss', help='Compute validation loss during training', dest='compute_val_loss', action='store_false')
 
     # Fit generator arguments
     parser.add_argument('--workers', help='Number of multiprocessing workers. To disable multiprocessing, set workers to 0', type=int, default=1)
@@ -491,6 +492,9 @@ def main(args=None):
         use_multiprocessing = True
     else:
         use_multiprocessing = False
+
+    if not args.compute_val_loss:
+        validation_generator = None
 
     # start training
     return training_model.fit_generator(
