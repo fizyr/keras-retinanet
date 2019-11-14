68fda1261083c1cb5cdbbeafe37be1a4c9591f8e
bfialkoff <bfialkoff@gmail.com>
2019-11-14 19:25:27 +0200

    iadded tuple unpack to recieve to args

--- keras_retinanet/callbacks/eval.py
+++ keras_retinanet/callbacks/eval.py
@@ -60,7 +60,7 @@ class Evaluate(keras.callbacks.Callback):
         logs = logs or {}
 
         # run evaluation
-        average_precisions = evaluate(
+        average_precisions, inference_time = evaluate(
             self.generator,
             self.model,
             iou_threshold=self.iou_threshold,
