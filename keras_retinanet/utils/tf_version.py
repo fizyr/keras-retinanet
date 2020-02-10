1532bddf5e54d6799052f9cd5788646bb78808e3
Hans Gaiser <h.gaiser@fizyr.com>
2020-02-10 17:05:19 +0100

    Allow older tensorflow versions.

--- keras_retinanet/utils/tf_version.py
+++ keras_retinanet/utils/tf_version.py
@@ -19,7 +19,11 @@ from __future__ import print_function
 import tensorflow as tf
 import sys
 
-MINIMUM_TF_VERSION = 2, 1, 0
+MINIMUM_TF_VERSION = 1, 14, 0
+BLACKLISTED_TF_VERSIONS = [
+    (2, 0, 0),  # Has a number of memory leaks and issues with eager execution.
+    (2, 0, 1),  # Has a number of memory leaks and issues with eager execution.
+]
 
 
 def tf_version():
@@ -30,18 +34,18 @@ def tf_version():
     return tuple(map(int, tf.version.VERSION.split('-')[0].split('.')))
 
 
-def tf_version_ok(minimum_tf_version=MINIMUM_TF_VERSION):
+def tf_version_ok(minimum_tf_version=MINIMUM_TF_VERSION, blacklisted=BLACKLISTED_TF_VERSIONS):
     """ Check if the current Tensorflow version is higher than the minimum version.
     """
-    return tf_version() >= minimum_tf_version
+    return tf_version() >= minimum_tf_version and tf_version() not in blacklisted
 
 
-def assert_tf_version(minimum_tf_version=MINIMUM_TF_VERSION):
+def assert_tf_version(minimum_tf_version=MINIMUM_TF_VERSION, blacklisted=BLACKLISTED_TF_VERSIONS):
     """ Assert that the Tensorflow version is up to date.
     """
     detected = tf.version.VERSION
     required = '.'.join(map(str, minimum_tf_version))
-    assert(tf_version() >= minimum_tf_version), 'You are using tensorflow version {}. The minimum required version is {}.'.format(detected, required)
+    assert(tf_version_ok(minimum_tf_version, blacklisted)), 'You are using tensorflow version {}. The minimum required version is {} (blacklisted: {}).'.format(detected, required, blacklisted)
 
 
 def check_tf_version():
