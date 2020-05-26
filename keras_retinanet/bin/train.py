46987089c1b01a3031998438e462add46c9ba3ce
sasha <sasha@phasegenomics.com>
2020-05-26 16:31:50 -0700

    making parameters for learning rate reduction upon loss plateau mutable by command line

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -190,8 +190,8 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
 
     callbacks.append(keras.callbacks.ReduceLROnPlateau(
         monitor    = 'loss',
-        factor     = 0.1,
-        patience   = 2,
+        factor     = args.reduce_lr_factor,
+        patience   = args.reduce_lr_patience,
         verbose    = 1,
         mode       = 'auto',
         min_delta  = 0.0001,
@@ -438,6 +438,8 @@ def parse_args(args):
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
     parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
     parser.add_argument('--compute-val-loss', help='Compute validation loss during training', dest='compute_val_loss', action='store_true')
+    parser.add_argument('--reduce-lr-patience', help='Reduce learning rate after validation loss decreases over reduce_lr_patience epochs', type=int, default=2)
+    parser.add_argument('--reduce-lr-factor', help='When learning rate is reduced due to reduce_lr_patience, multiply by reduce_lr_factor', type=float, default=0.1)
 
     # Fit generator arguments
     parser.add_argument('--multiprocessing',  help='Use multiprocessing in fit_generator.', action='store_true')
