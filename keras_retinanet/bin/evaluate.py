ede623447e460ef706f06127a1ff106613da36f2
Lampros Mouselimis <mouselimislampros@gmail.com>
2020-07-13 14:05:19 +0300

    remove whitespaces

--- keras_retinanet/bin/evaluate.py
+++ keras_retinanet/bin/evaluate.py
@@ -113,7 +113,7 @@ def parse_args(args):
     parser.add_argument('--max-detections',   help='Max Detections per image (defaults to 100).', default=100, type=int)
     parser.add_argument('--save-path',        help='Path for saving images with detections (doesn\'t work for COCO).')
     parser.add_argument('--image-min-side',   help='Rescale the image so the smallest side is min_side.', type=int, default=800)
-    parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)    
+    parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)
     parser.add_argument('--no-resize',        help='Don''t rescale the image.', action='store_true')
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file (only used with --convert-model).')
 
