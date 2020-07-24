0bffa4897d9925c5243c1fd2808f3ce1087fe10b
cboden <cbodenstein@gmx.de>
2020-07-24 20:38:20 +0200

    use resnet 0.2.0

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -126,7 +126,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
             'regression'    : losses.smooth_l1(),
             'classification': losses.focal()
         },
-        optimizer=keras.optimizers.adam(lr=lr, clipnorm=0.001)
+        optimizer=keras.optimizers.Adam(lr=lr, clipnorm=0.001)
     )
 
     return model, training_model, prediction_model
