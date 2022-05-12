8a66af9c7dda7bd93944c90d1ff32dee7f99d956
Vipsap <88127737+Vipsap@users.noreply.github.com>
2022-05-12 23:42:07 +0530

    Update pascal_voc.py

--- keras_retinanet/preprocessing/pascal_voc.py
+++ keras_retinanet/preprocessing/pascal_voc.py
@@ -28,26 +28,8 @@ except ImportError:
     import xml.etree.ElementTree as ET
 
 voc_classes = {
-    'aeroplane'   : 0,
-    'bicycle'     : 1,
-    'bird'        : 2,
-    'boat'        : 3,
-    'bottle'      : 4,
-    'bus'         : 5,
-    'car'         : 6,
-    'cat'         : 7,
-    'chair'       : 8,
-    'cow'         : 9,
-    'diningtable' : 10,
-    'dog'         : 11,
-    'horse'       : 12,
-    'motorbike'   : 13,
-    'person'      : 14,
-    'pottedplant' : 15,
-    'sheep'       : 16,
-    'sofa'        : 17,
-    'train'       : 18,
-    'tvmonitor'   : 19
+    'pdf_img'   : 0,
+   
 }
 
 
